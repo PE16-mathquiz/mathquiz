9a82c979250c1b224d5dce5e03f409883779d32f
yamsho <yamsho102@uec.ac.jp>
2016-12-20 23:42:06 +0900

    Make model

---
+++ Model.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+class Model {
+	private int dec_int; //ランダムに決める問題の数(10進数)
+	private String dec; //10進数の問題の数
+	private String bin; //2進数の問題の数
+	private String oct; //8進数の問題の数
+	
+	//コンストラクタ
+	public Model(){
+		dec_int = (int)(Math.random() * Math.pow(2, 10));
+		dec = String.valueOf(dec_int);
+		bin = Integer.toBinaryString(dec_int);
+		oct = Integer.toOctalString(dec_int);
+	}
+	
+	//print関数
+	public void print_dec(){
+		System.out.println("10進数: " + dec);
+	}
+	public void print_bin(){
+		System.out.println(" 2進数: " + bin);
+	}
+	public void print_oct(){
+		System.out.println(" 8進数: " + oct);
+	}
+}
+
+class ModelMain{
+	public static void main(String[] args){
+		Model sample = new Model();
+		sample.print_dec();
+		sample.print_bin();
+		sample.print_oct();
+	}
+}
