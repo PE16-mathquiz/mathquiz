9a3a5a80e4381894eca4e1e82543857bd5ededb6
yamsho <yamsho102@uec.ac.jp>
2017-01-05 14:49:34 +0900

    Arrange indent of Model.java

--- Model.java
+++ Model.java
@@ -22,9 +22,9 @@ class Model extends Observable{
     //コンストラクタ
     public Model(int q, int s){
     	this.que = q;
-	    this.sol = s;
+	this.sol = s;
 	    
-	    this.reset();
+	this.reset();
     }
     
     //set関数
@@ -43,58 +43,58 @@ class Model extends Observable{
     
     //print関数
     public void print_all(){
-		System.out.println("10進数: " + dec);
-		System.out.println(" 2進数: " + bin);
-		System.out.println(" 8進数: " + oct);
-		System.out.println("16進数: " + hex);
+	System.out.println("10進数: " + dec);
+	System.out.println(" 2進数: " + bin);
+	System.out.println(" 8進数: " + oct);
+	System.out.println("16進数: " + hex);
     }
     
     //問題をリセットする関数(コンストラクタでも使用)
     public void reset(){
     	dec_int = (int)(Math.random() * Math.pow(2, 10));
-	    dec = String.valueOf(dec_int);
-	    bin = Integer.toBinaryString(dec_int);
-	    oct = Integer.toOctalString(dec_int);
-	    hex = Integer.toHexString(dec_int).toUpperCase();
-	    quejp = String.format("次の%d進数を%d進数に変換しなさい", que, sol);
+	dec = String.valueOf(dec_int);
+	bin = Integer.toBinaryString(dec_int);
+	oct = Integer.toOctalString(dec_int);
+	hex = Integer.toHexString(dec_int).toUpperCase();
+	quejp = String.format("次の%d進数を%d進数に変換しなさい", que, sol);
 	    
-	    quenum = "none";
-	    switch(que){
-		case 2:
-	    	quenum = String.format("%10s", bin).replace(' ', '0');
-	    	break;
-		case 8:
-	    	quenum = "0" + oct;
-	    	break;
-		case 10:
-	    	quenum = dec;
-	   	 	break;
-		case 16:
-	    	quenum = "0x" + hex;
-	    	break;
-	    }
-		
-		answer = "none";
-		switch(sol){
-		case 2:
-	    	answer = bin;
-	    	break;
-		case 8:
-	    	answer = oct;
-	    	break;
-		case 10:
-	    	answer = dec;
-	    	break;
-		case 16:
-	    	answer = hex;
-	    	break;
-		}
+	quenum = "none";
+	switch(que){
+	case 2:
+	    quenum = String.format("%10s", bin).replace(' ', '0');
+	    break;
+	case 8:
+	    quenum = "0" + oct;
+	    break;
+	case 10:
+	    quenum = dec;
+	    break;
+	case 16:
+	    quenum = "0x" + hex;
+	    break;
+	}
+	
+	answer = "none";
+	switch(sol){
+	case 2:
+	    answer = bin;
+	    break;
+	case 8:
+	    answer = oct;
+	    break;
+	case 10:
+	    answer = dec;
+	    break;
+	case 16:
+	    answer = hex;
+	    break;
+	}
     }
-
+    
     //問題を表示する関数
     public void print_q(){
-		System.out.println(quejp);
-		System.out.println(quenum);
+	System.out.println(quejp);
+	System.out.println(quenum);
     }
 
     //答えを入力してもらって確認する
@@ -102,11 +102,11 @@ class Model extends Observable{
     	String reply = r.toUpperCase();  //入力された16進数が小文字でも正解と判定するため
     	
     	if(reply.equals(answer)){
-	    	System.out.println("正解!!");
-		}
-		else{
-	    	System.out.println("不正解..");
-		}
+	    System.out.println("正解!!");
+	}
+	else{
+	    System.out.println("不正解..");
+	}
     }
 }
 
@@ -114,125 +114,125 @@ class ModelMain{
     public static void main(String[] args){
     	int question = 10;
     	int solution = 16;
-		Model model = new Model(question, solution);
-		String reply = "none";
-		Scanner scan = new Scanner(System.in);
-		int reset_flag; //問題をリセットするかどうかのフラグ
-		int change_flag; //基数を変更するかどうかのフラグ
-		boolean q_flag, s_flag; //基数が入力されたどうかのフラグ
+	Model model = new Model(question, solution);
+	String reply = "none";
+	Scanner scan = new Scanner(System.in);
+	int reset_flag; //問題をリセットするかどうかのフラグ
+	int change_flag; //基数を変更するかどうかのフラグ
+	boolean q_flag, s_flag; //基数が入力されたどうかのフラグ
+	
+	do{
+	    //フラグの初期化
+	    reset_flag = -1;
+	    change_flag = -1;
+	    q_flag = false;
+	    s_flag = false;
+	    
+	    //答えの表示(デバッグ用)
+	    model.print_all();
+	    model.print_q();
+	    
+	    //答えの入力
+	    System.out.println("答えを入力してください");
+	    reply = scan.next();
+	    model.ans_q(reply);
+	    
+	    //やり直すかの確認
+	    do{
+		String reset_c = "none";
+		System.out.println("もう一度やりますか?");
+		System.out.println("はい: \"y\" or \"yes\" / いいえ: \"n or \"no");
+		reset_c = scan.next();
+		
+		if(reset_c.equals("y") || reset_c.equals("yes")){
+		    reset_flag = 1;
+		}
+		else if(reset_c.equals("n") || reset_c.equals("no")){
+		    reset_flag = 0;
+		}
+	    }while(reset_flag < 0);
+	    
+	    if(reset_flag != 1){
+		break;
+	    }
+	    
+	    //基数を変更するかの確認
+	    do{
+		String change_c = "none";
+		System.out.println("問題の基数を変更しますか?");
+		System.out.println("はい: \"y\" or \"yes\" / いいえ: \"n or \"no");
+		change_c = scan.next();
+		
+		if(change_c.equals("y") || change_c.equals("yes")){
+		    change_flag = 1;
+		}
+		else if(change_c.equals("n") || change_c.equals("no")){
+		    change_flag = 0;
+		}
+	    }while(change_flag < 0);
+	    
+	    //変更する場合を入力を要請する
+	    if(change_flag == 1){
+		do{
+		    String que_c = "none";
+		    System.out.println("問題の基数を入力してください");
+		    System.out.printf("現在の問題の基数: %d\n", question);
+		    System.out.println("2 or 8 or 10 or 16 (整数のみを入力)");
+		    que_c = scan.next();
+		    
+		    try{
+			int temp_q = Integer.parseInt(que_c);
+			
+			switch(temp_q){
+			case 2:
+			case 8:
+			case 10:
+			case 16:
+			    question = temp_q;
+			    q_flag = true;
+			    break;
+			default:
+			    break;
+			}
+		    }
+		    catch(NumberFormatException e){
+			System.out.println("整数のみを入力してください\n");
+			continue;
+		    }
+		}while(!q_flag);
 		
 		do{
-			//フラグの初期化
-			reset_flag = -1;
-			change_flag = -1;
-			q_flag = false;
-			s_flag = false;
+		    String sol_c = "none";
+		    System.out.println("答えの基数を入力してください");
+		    System.out.printf("現在の答えの基数: %d\n", solution);
+		    System.out.println("2 or 8 or 10 or 16 (整数のみを入力)");
+		    sol_c = scan.next();
+		    
+		    try{
+			int temp_s = Integer.parseInt(sol_c);
 			
-			//答えの表示(デバッグ用)
-			model.print_all();
-			model.print_q();
-			
-			//答えの入力
-			System.out.println("答えを入力してください");
-			reply = scan.next();
-			model.ans_q(reply);
-			
-			//やり直すかの確認
-			do{
-				String reset_c = "none";
-				System.out.println("もう一度やりますか?");
-				System.out.println("はい: \"y\" or \"yes\" / いいえ: \"n or \"no");
-				reset_c = scan.next();
-				
-				if(reset_c.equals("y") || reset_c.equals("yes")){
-					reset_flag = 1;
-				}
-				else if(reset_c.equals("n") || reset_c.equals("no")){
-					reset_flag = 0;
-				}
-			}while(reset_flag < 0);
-			
-			if(reset_flag != 1){
-				break;
+			switch(temp_s){
+			case 2:
+			case 8:
+			case 10:
+			case 16:
+			    solution = temp_s;
+			    s_flag = true;
+			    break;
+			default:
+			    break;
 			}
-			
-			//基数を変更するかの確認
-			do{
-				String change_c = "none";
-				System.out.println("問題の基数を変更しますか?");
-				System.out.println("はい: \"y\" or \"yes\" / いいえ: \"n or \"no");
-				change_c = scan.next();
-				
-				if(change_c.equals("y") || change_c.equals("yes")){
-					change_flag = 1;
-				}
-				else if(change_c.equals("n") || change_c.equals("no")){
-					change_flag = 0;
-				}
-			}while(change_flag < 0);
-			
-			//変更する場合を入力を要請する
-			if(change_flag == 1){
-				do{
-					String que_c = "none";
-					System.out.println("問題の基数を入力してください");
-					System.out.printf("現在の問題の基数: %d\n", question);
-					System.out.println("2 or 8 or 10 or 16 (整数のみを入力)");
-					que_c = scan.next();
-					
-					try{
-						int temp_q = Integer.parseInt(que_c);
-						
-						switch(temp_q){
-						case 2:
-						case 8:
-						case 10:
-						case 16:
-							question = temp_q;
-							q_flag = true;
-							break;
-						default:
-							break;
-						}
-					}
-					catch(NumberFormatException e){
-						System.out.println("整数のみを入力してください\n");
-						continue;
-					}
-				}while(!q_flag);
-				
-				do{
-					String sol_c = "none";
-					System.out.println("答えの基数を入力してください");
-					System.out.printf("現在の答えの基数: %d\n", solution);
-					System.out.println("2 or 8 or 10 or 16 (整数のみを入力)");
-					sol_c = scan.next();
-					
-					try{
-						int temp_s = Integer.parseInt(sol_c);
-						
-						switch(temp_s){
-						case 2:
-						case 8:
-						case 10:
-						case 16:
-							solution = temp_s;
-							s_flag = true;
-							break;
-						default:
-							break;
-					}
-					}
-					catch(NumberFormatException e){
-						System.out.println("整数のみを入力してください\n");
-						continue;
-					}					
-				}while(!s_flag);
-				
-				model.set_base(question, solution);
-			}
-			
-			model.reset();
-		}while(reset_flag == 1);
+		    }
+		    catch(NumberFormatException e){
+			System.out.println("整数のみを入力してください\n");
+			continue;
+		    }					
+		}while(!s_flag);
+		
+		model.set_base(question, solution);
+	    }
+	    
+	    model.reset();
+	}while(reset_flag == 1);
     }
 }
